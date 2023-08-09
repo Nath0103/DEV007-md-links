59001874e50b10bb9d46b4587c6c2f68d5e909a7
Nathaly Santos <nathalysantos0103@gmail.com>
2023-08-09 13:04:09 -0500

    test a funcion esArchivoMD

--- funcions.js
+++ funcions.js
@@ -1,12 +1,12 @@
-import path from'path';
-import fs from'fs';
+import path from "path";
+import fs from "fs";
 // Validar la ruta
 function validarRuta(ruta) {
   if (fs.existsSync(ruta)) {
     console.log("----La ruta existe.----");
     return true;
   } else {
-   // console.log("----La ruta no existe----");
+    // console.log("----La ruta no existe----");
     return false;
   }
 }
@@ -14,60 +14,47 @@ function validarRuta(ruta) {
 function rutaAbsolute(ruta) {
   const rutaAbsoluta = path.resolve(ruta); //ya pasamos la ruta a absoluta
   console.log(rutaAbsoluta);
-  return rutaAbsoluta 
+  return rutaAbsoluta;
 }
 //::::::::::::::::::::::RECURSIVIDAD:::::::::::::::::::::::::::::::::::::::
-function directorioOArchivo(ruta) {
-  let arrayArchivos=[];
-  const contenedorArchivos=fs.statSync(ruta);
-  if (contenedorArchivos.isFile()) {
-    arrayArchivos.push(ruta);
-  }else if (contenedorArchivos.isDirectory()) {
-    const archivos = fs.readdirSync(ruta, 'utf-8');
-    archivos.forEach(archivo => {
-      const directorioNuevo = path.join(ruta,archivo);
-     if (directorioNuevo.isDirectory()) {
-       arrayArchivos.push(ruta);
-     }
-    });
-  }
-
-}
-
-
-//Comprobar si es un Directorio
-function directorio(ruta){
-  const r = fs.lstatSync(ruta);
-  return r.isDirectory() ? true : false;//true
-}
-
-//
-//Comprobar si es un Archivo
-function archivo(ruta){
-  const a = fs.lstatSync(ruta);
-  if (a.isFile()) {
-    console.log("La ruta es de un Archivo");
-  }else{
-    console.log("La ruta NO es un archivo");
-  }
-
-}
 // pregunta si el archivo es MD
 function esArchivoMD(ruta) {
   const extension = path.extname(ruta);
-  if (extension === ".md") {
-    console.log("si es un md ()");
-  }else{
-    console.log("----No es un md ()----");
-  }
+  return extension === ".md";
 }
+
+function directorioOArchivo(ruta) {
+  let arrayArchivos = [];
+
+  const contenedorArchivos = fs.statSync(ruta);
+  if (contenedorArchivos.isFile()) {
+    if (esArchivoMD(ruta)) {
+      arrayArchivos.push(ruta);
+    }
+  } else if (contenedorArchivos.isDirectory()) {
+    const archivos = fs.readdirSync(ruta, "utf-8");
+    archivos.forEach((archivo) => {
+      const directorioNuevo = path.join(ruta, archivo);
+      const contenedorNuevo = fs.statSync(directorioNuevo);
+      if (contenedorNuevo.isFile()) {
+        if (esArchivoMD(directorioNuevo)) {
+          arrayArchivos.push(directorioNuevo);
+        }
+      } else if (contenedorNuevo.isDirectory()) {
+        const archivosEnDirectorios = directorioOArchivo(directorioNuevo);
+        arrayArchivos.push(...archivosEnDirectorios.filter(esArchivoMD));
+      }
+    });
+  }
+  console.log(arrayArchivos);
+  return arrayArchivos;
+}
+
 // -------------------------------------------------
 
 export {
   validarRuta,
   rutaAbsolute,
-  directorio,
-  archivo,
   esArchivoMD,
-  obtenerArchivosMDEnDirectorio
+  directorioOArchivo,
 };
